01a78d1003a474518481cb671609b7666317caaf
qiaoximing <qiaoximing@frontis.cn>
2023-05-19 14:48:52 +0800

    u

--- js/test.js
+++
@@ -1,2 +0,0 @@
-
-javascript:var root = document.documentElement;var s = window.getComputedStyle(root).getPropertyValue('filter');root.style.setProperty('filter',s=='none'?'grayscale(1)':'none');void(0);
